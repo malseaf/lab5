d2ef564bfaa313741e81af41438d6a5a50444bd4
malak <malseaf@andrew.cmu.edu>
2021-02-21 13:41:07 +0300

    completed the home model with the controller and veiw

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,13 @@
 class Quote < ApplicationRecord
+
+#validations
+validates_presence_of :body
+validates_presence_of :source
+# Method to get a random quote for home page
+#
+# @return [Quote]
+# the random quote
+def self.get_random_quote
+    self.all.sample
+   end
 end
